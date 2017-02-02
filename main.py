e7ec15c1a57ff3e041b2a1da3b2ec02653ce1184
Grant Patterson <grantrpatterson@gmail.com>
2017-02-02 17:49:04 -0600

    first commit after finishing code

--- main.py
+++ main.py
@@ -1,20 +1,76 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FlieSystemLoader(template_dir),
+                                autoescape = True)
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")      #a username a through z zero through nine with a length between three to twenty characters
+def valid_username(username):
+    return username and USER_RE.match(username)     #return if a match
+
+PASS_RE = re.compile(r"^.{3,20}$")                  #any characters with a length between three and twenty characters
+def valid_password(password):
+    return password and PASS_RE.match(password)     #return if a match
+
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)       #return if a match
+
+class Signup(BaseHandler):
+
+    def get(self):
+        self.render("signup-form.html")
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        params = dict(username = username,
+                      email = email)
+
+        if not valid_username(username):
+            params['error_username'] = "The username entered is not valid.."
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "The password entered is not valid.."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "The second password entered does not match.."
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "The email entered is not valid.."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            self.redirect('/lc101/welcome?username=' + username)
+
+class Welcome(BaseHandler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username = username)
+        else:
+            self.redirect('/user-signup/signup')
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -23,3 +79,6 @@ class MainHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+app = webapp2.WSGIApplication( [('/user-signup/signup', Signup)
+                                ('/user-signup/welcome', Welcome)],
+                              debug = True)
